79847ed398820169706fcd8c9ca8489b854d259b
Brian <bstanton773@gmail.com>
2021-03-15 15:14:37 -0500

    Monday afternoon work

--- src/App.js
+++ src/App.js
@@ -6,6 +6,32 @@ import Contact from './views/Contact';
 import Home from './views/Home';
 
 export default class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name: "Brian Stanton",
+      racers: []
+    }
+  }
+
+  handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(e);
+    let year = e.target.year.value;
+    let season = e.target.season.value;
+    fetch(`https://ergast.com/api/f1/${year}/${season}/driverStandings.json`)
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                racers: data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+            })
+        })
+        .catch(error => console.log(error))
+  }
+
+  
+
   render() {
     return (
       <div>
@@ -13,9 +39,9 @@ export default class App extends Component {
         <Navbar />
         <main className="container">
           <Switch>
-            <Route exact path="/" render={() => <Home />} />
-            <Route path="/about" render={() => <About />} />
-            <Route path="/contact" render={() => <Contact />} />
+            <Route exact path="/" render={() => <Home name={this.state.name} racers={this.state.racers} handleSubmit={this.handleSubmit} />} />
+            <Route path="/about" render={() => <About name={this.state.name} />} />
+            <Route path="/contact" render={() => <Contact name={this.state.name} />} />
           </Switch>
         </main>
       </div>
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
+    
+
+
     render() {
         return (
             <div>
                 This is the Home Page
+                <h3>Welcome, {this.props.name}</h3>
+                <form onSubmit={(e) => this.props.handleSubmit(e)}>
+                    <input type="text" className="form-control" name="year" placeholder="Year" />
+                    <input type="text" className="form-control" name="season" placeholder="Season" />
+                    <button type="submit" className="btn btn-outline-info">Submit</button>
+                </form>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Position</th>
+                            <th>Points</th>
+                            <th>Wins</th>
+                            <th>Name</th>
+                            <th>DOB</th>
+                            <th>Nationality</th>
+                            <th>Constructor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.racers.map((racer, index) => (
+                            <tr key={index}>
+                                <td>{racer.position}</td>
+                                <td>{racer.points}</td>
+                                <td>{racer.wins}</td>
+                                <td>{racer.Driver.givenName} {racer.Driver.familyName}</td>
+                                <td>{racer.Driver.dateOfBirth}</td>
+                                <td>{racer.Driver.nationality}</td>
+                                <td>{racer.Constructors[0].name}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         )
     }
